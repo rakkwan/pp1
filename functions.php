4880dff21cc9f518f1827c0b811898a8531e1025
Jittima Goodrich <jgoodrich4@mail.greenriver.edu>
2019-04-03 17:38:41 -0400

    Added two php files to pp1

---
+++ functions.php
@@ -0,0 +1,22 @@
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function printArr($numbers)
+{
+    foreach ($numbers as $num)
+    {
+        echo $num;
+    }
+
+}
+
+function largest($numbers)
+{
+
+}
+
+
+?>;
---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program</title>
+</head>
+<body>
+    <h1>Pair Program</h1>
+
+    <?php
+
+    $numbers = array(7, 9, 8, 9, 8, 8, 6);
+    include("functions.php");
+
+    printArr($numbers);
+    ?>;
+
+
+</body>
+</html>
